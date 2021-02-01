457200ee3d9691d67f36ffb62b4c5852125472f7
leducduweb <78273517+leducduweb@users.noreply.github.com>
2021-02-01 19:43:59 +0100

    Delete spacex directory

--- spacex/script.js
+++
@@ -1,8 +0,0 @@
-const card = $(".perspective__3d");
-
-$("body").on("mousemove", function (t){
-    let vertical = -($(window).innerWidth()/ 2 - t.pageX) /70,
-        horizontal = ($(window).innerHeight()/ 2 - t.pageY)/ 70;
-
-    card.attr("style", "transform: rotateY(" + vertical + "deg) rotateX(" + horizontal + "deg)")
-});
